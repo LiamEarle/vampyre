c009223275bb709a1d7f2080b5fc6ac0ed14b53c
LiamEarle <liam.wilkinson@gmail.com>
2021-02-05 08:23:03 +1000

    Pylint & Black
    * Added pylint linter and black code formatter to project
    * Update README.me with usage

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# vampyre
+# Vampyre
 A Python CLI for manipulating IP addresses
+
+## Usage
+```
+usage: vampyre.py [-h] [--version] [--outfile [OUTFILE]] [--infile [INFILE]] [--fangs [FANGS]] [--validate] {fang,defang,extract} ...
+
+Vampyre - IP Manipulation CLI
+
+positional arguments:
+  {fang,defang,extract}
+    fang                Add 'fangs' to input
+    defang              Remove 'fangs' from input
+    extract             Extract IPv4 addresses from input
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --version             show program's version number and exit
+  --outfile [OUTFILE]   Specify the output file
+  --infile [INFILE]     Specify the input file
+  --fangs [FANGS]       Specify the fang characters
+  --validate            Validate IPv4 addresses
+```
--- src/vampyre.py
+++ src/vampyre.py
@@ -1,88 +1,101 @@
-#!/usr/bin/env python
+"""
+vampyre.py - an IPv4 manipulation CLI
+
+python3 vampyre.py --help
+"""
 
 import argparse
 import sys
 import re
-import io
 import ipaddress
 
-# Variables
-IP_MATCH_REGEX = re.compile(r'(\d+\.\d+\.\d+\.\d+)')
+# Globals
+VAMPYRE_VERSION = "0.1.0-dev"
 
-parser = argparse.ArgumentParser(description='Vampyre - IP Manipulation CLI')
-subparsers = parser.add_subparsers(dest='command')
+# Variables
+IP_MATCH_REGEX = re.compile(r"(\d+\.\d+\.\d+\.\d+)")
+
+parser = argparse.ArgumentParser(description="Vampyre - IP Manipulation CLI")
+subparsers = parser.add_subparsers(dest="command")
 
 # Standard Arguments
-parser.version = '0.1.0-dev'
-parser.add_argument('--version', action='version')
-parser.add_argument('--outfile', nargs='?', type=argparse.FileType('w'), help='Specify the output file')
-parser.add_argument('--infile', nargs='?', type=argparse.FileType('r'), help='Specify the input file')
-parser.add_argument('--fangs', nargs='?', type=str, default='[.]', help='Specify the fang characters')
-parser.add_argument('--validate', action='store_true', help='Validate IPv4 addresses')
-
+parser.version = VAMPYRE_VERSION
+parser.add_argument("--version", action="version")
+parser.add_argument(
+    "--outfile", nargs="?", type=argparse.FileType("w"), help="Specify the output file"
+)
+parser.add_argument(
+    "--infile", nargs="?", type=argparse.FileType("r"), help="Specify the input file"
+)
+parser.add_argument(
+    "--fangs", nargs="?", type=str, default="[.]", help="Specify the fang characters"
+)
+parser.add_argument("--validate", action="store_true", help="Validate IPv4 addresses")
 
 # Fang Parser
-fang_parser = subparsers.add_parser(name='fang', help='Add \'fangs\' to input')
-fang_parser.add_argument('input', nargs='*')
+fang_parser = subparsers.add_parser(name="fang", help="Add 'fangs' to input")
+fang_parser.add_argument("input", nargs="*")
 
 # Defang Parser
-defang_parser = subparsers.add_parser(name='defang', help='Remove \'fangs\' from input')
-defang_parser.add_argument('input', nargs='*')
+defang_parser = subparsers.add_parser(name="defang", help="Remove 'fangs' from input")
+defang_parser.add_argument("input", nargs="*")
 
 # Extract Parser
-extract_parser = subparsers.add_parser(name='extract', help='Extract IPv4 addresses from input')
-extract_parser.add_argument('input', nargs='*')
+extract_parser = subparsers.add_parser(
+    name="extract", help="Extract IPv4 addresses from input"
+)
+extract_parser.add_argument("input", nargs="*")
 
 args = parser.parse_args()
 
+# Check is parser has a command, if not - exit.
+if args.command is None:
+    parser.print_help()
+    sys.exit(1)
+
 # Functions
 
-
-addresses = [] # Variable for holding matched IPv4 addresses
 candidates = []
 
 # Read from stdin and add to candidates
-if (sys.stdin):
+if not sys.stdin.isatty():
     data = sys.stdin.read()
     candidates.append(data)
 
 # Read infile and add to candidates
-if (args.infile):
+if args.infile:
     data = args.infile.read()
     candidates.append(data)
 
 # Read arguments and add to candidates
-if (len(args.input) > 0):
+if args.input:
     candidates.extend(args.input)
 
-# Check all candidates for IPv4 addresses and validate if the flag is specified.
-for addr in candidates:
-    matches = IP_MATCH_REGEX.findall(addr)
+if args.command == "fang":
+    print("Fang was chosen")
+elif args.command == "defang":
+    print("Defang was chosen")
+elif args.command == "extract":
+    # Check all candidates for IPv4 addresses and validate if the flag is specified.
+    addresses = []
 
-    if (args.validate):
-        for match in matches:
-            try:
-                ipaddress.ip_address(match) # NO-OP: This will throw an error if the address is not valid
-                addresses.append(match)
-            except ValueError:
-                print(f'[!] {match} is not a valid IPv4 address.', file=sys.stderr)
-    else:
-        addresses.extend(matches)
+    for addr in candidates:
+        matches = IP_MATCH_REGEX.findall(addr)
 
-if (args.command == 'fang'):
-    print('Fang was chosen')
-elif (args.command == 'defang'):
-    print('Defang was chosen')
-elif (args.command == 'extract'):
-    if (args.outfile):
+        if args.validate:
+            for match in matches:
+                try:
+                    ipaddress.ip_address(match)
+                    addresses.append(match)
+                except ValueError:
+                    print(f"[!] {match} is not a valid IPv4 address.", file=sys.stderr)
+        else:
+            addresses.extend(matches)
+
+    if args.outfile:
         outfile = args.outfile
         for addr in addresses:
-            outfile.write(f'{addr}\n')
+            outfile.write(f"{addr}\n")
         outfile.close()
     else:
         print(addresses)
-        
-
-
-
-
